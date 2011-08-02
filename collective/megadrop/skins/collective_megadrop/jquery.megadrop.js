20ada06ea700fa750b6a543031bd1b982289467c
Luke Brannon <luke.brannon@gmail.com>
2011-08-02 16:10:45 -0700

    updated jquery.megadrop.js to properly close tabs
     Please enter the commit message for your changes. Lines starting

--- collective/megadrop/skins/collective_megadrop/jquery.megadrop.js
+++ collective/megadrop/skins/collective_megadrop/jquery.megadrop.js
@@ -1,14 +1,21 @@
-/*
-Sagarika Gogikari <gsagarika2012@gmail.com>
-*/
-
 $(document).ready(function()
 {
   //hide the all of the element with class msg_body
-  
+
   $("#portal-globalnav a").click(
         function(){
-    $("#portal-globalnav a").parent('h2').next('div').hide();
-    $(this).parent().next('div').slideDown(600);
+        var className = "." + $(this).attr('id');
+
+
+
+  if ($(className).hasClass('megatrack')) {
+  $('.megatrack').slideUp(600).addClass('megahide');
+  $('.megatrack').removeClass('megatrack');
+  } else {
+
+  $('.megatrack').slideUp(5).addClass('megahide');
+  $('.megatrack').removeClass('megatrack');
+
+  $(className).slideDown(600).removeClass('megahide').addClass('megatrack');}
   });
 });
