4d4537c1ac516f36a0d4ce23583c3772b690737e
Luke Brannon <luke.brannon@gmail.com>
2012-01-25 21:55:10 -0800

    added IRichTextMegaDrop interface

--- collective/megadrop/browser/interfaces.py
+++ collective/megadrop/browser/interfaces.py
@@ -1,5 +1,12 @@
 from plone.theme.interfaces import IDefaultPloneLayer
+from zope.interface import Interface
+
 
 class IMegaDropInstalled(IDefaultPloneLayer):
     """A layer specific for collective.megadrop.
+    """
+    
+class IRichTextMegaDrop(Interface):
+    """Marker interface used to trigger RichText 
+       MegaDrop menu.
     """
